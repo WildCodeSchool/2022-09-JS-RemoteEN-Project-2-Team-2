c5b03441d43e2959e2d8e3715a055022bd4fee86
Pierre Basck <pierre.basck@gmail.com>
2022-10-26 11:49:07 +0200

    changed icons

--- frontend/src/components/CurrentWeather/CurrentWeather.jsx
+++ frontend/src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./CurrentWeather.css";
-import sunny from "../../assets/icons/sunny.svg";
+import clearSkyD from "../../assets/icons/clear-sky-.svg";
 
 function CurrentWeather() {
   return (
     <div>
       <div className="weatherMain">
-        <img className="imgWeather" src={sunny} alt="Sunny" />
+        <img className="imgWeather" src={clearSkyD} alt="Sunny" />
         <p className="myCityTem">16°</p>
         <p className="myWeather">Cloudy</p>
       </div>
