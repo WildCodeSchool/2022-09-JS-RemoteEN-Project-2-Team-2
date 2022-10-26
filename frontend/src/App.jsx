29f6a1bda39c2bb22cd57db660522583ba9a65c7
Pierre Basck <pierre.basck@gmail.com>
2022-10-26 10:57:17 +0200

    App Component added + Removed CurrentWeather

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import Home from "@pages/Home";
-
+import React from "react";
 import "./App.css";
+import sunny from "./assets/icons/sunny.svg";
+import logo from "./assets/logo/suni_logo.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou App</p>
+    <div>
+      <div className="weatherMain">
+        <p className="myCity">Hamburg</p>
+        <p className="myDate">
+          26 October, <span className="weekDay">Wednesday</span>
+        </p>
+        <div className="myFooter">
+          <img className="sunnyLogo" src={logo} alt="" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
