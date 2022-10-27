17b8bc8f2f01c06d76f6b3ca0e103af86041abfc
Pauline Kraus <kraus.pauline1@gmail.com>
2022-10-27 09:45:20 +0200

    background bi color css half fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import MainContainer from "./components/main-container/main-container";
 import SavedLocations from "./components/SavedLocations/SavedLocations";
 
 import "./App.css";
@@ -7,7 +6,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <MainContainer />
       <SavedLocations />
     </div>
   );
--- frontend/src/components/SavedLocations/SavedLocations.jsx
+++ frontend/src/components/SavedLocations/SavedLocations.jsx
@@ -2,23 +2,32 @@ import React from "react";
 import button from "../../assets/Button.png";
 import Cities from "../Cities/Cities";
 import "./SavedLocations.css";
+import Rectangle from "../../assets/Rectangle.png";
 
 function SavedLocations() {
+  const background = {
+    backgroundImage: `url(${Rectangle})`,
+    backgroundRepeat: "repeat-y",
+    backgroundSize: "50% auto",
+    ZIndex: "3",
+  };
   return (
     <div>
       <div className="savedlocations-box">
-        <div className="button-flexbox">
-          <button className="adding-saved-locations" type="button">
-            <img src={button} alt="" />
-          </button>
-        </div>
-        <h3 className="saved-title">SAVED LOCATIONS</h3>
-        <div className="flex-box-container">
-          <div className="saved-cities-components">
-            <Cities />
-            <Cities />
-            <Cities />
-            <Cities />
+        <div style={background}>
+          <div className="button-flexbox">
+            <button className="adding-saved-locations" type="button">
+              <img src={button} alt="" />
+            </button>
+          </div>
+          <h3 className="saved-title">SAVED LOCATIONS</h3>
+          <div className="flex-box-container">
+            <div className="saved-cities-components">
+              <Cities />
+              <Cities />
+              <Cities />
+              <Cities />
+            </div>
           </div>
         </div>
       </div>
