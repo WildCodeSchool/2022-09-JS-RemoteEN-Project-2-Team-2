ccdb0ff8c02d852d11f47f96ac669fe59debe36e
Pierre Basck <pierre.basck@gmail.com>
2022-10-28 08:09:18 +0200

    changed pixels to width on SubButton and added hom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import Home from "@pages/Home";
+import SubscribeBtn from "@components/subscribe/SubscribeBtn";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou sub test</p>
+      <SubscribeBtn />
     </div>
   );
 }
