70218d3c9f0de2a82f3eec0580e8c15b9a80f6dc
RussellCorpuz <russelljames.fr@icloud.com>
2022-11-16 15:54:03 +0100

    firstcommit back end.

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,14 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
+const weatherController = require("./controllers/weatherController");
+
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.post("/weather/subscribe", weatherController.subscribe);
 
 module.exports = router;
